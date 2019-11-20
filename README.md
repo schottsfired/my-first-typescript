5b503dbee6d95562730757e6c5b8ca37895fbd4c
David Schott <schottdb@gmail.com>
2019-11-20 08:55:18 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# My First TypeScript
+
+## Getting Started
+Install Node on OSX with `brew install node`
+
+Install TypeScript with `npm install -g typescript` (g for global)
+
+Compile a `.ts` file with `tsc file.ts` (this turns the file into JavaScript `.js`)
+
+Auto-compile a `.ts` file with `tsc file.ts -w` (w for watch)
+
+Include compiled `.js` file in `index.html`, then open in browser. Use dev tools to view console output of TypeScript (JavaScript) program.
---
+++ types.ts
@@ -0,0 +1,31 @@
+let myString: string;
+let myNum: number;
+let myBool: boolean;
+let myVar: any;
+
+// let strArr: string[];
+// let numArr: number[];
+// let boolArr: boolean[];
+
+let strArr: Array<string>;
+let numArr: Array<number>;
+let boolArr: Array<boolean>;
+
+let strNumTuple: [string, number];
+
+myString = 'Hello'.slice(0, 3);
+myNum = 1;
+myBool = true;
+myVar = 5;
+
+strArr = ['Hello', 'World'];
+numArr = [1, 2, 3];
+boolArr = [true, false, true];
+
+strNumTuple = ['Hello', 4];
+
+let myVoid: void = undefined;
+let myNull: null = null;
+let myUndefined: undefined = undefined;
+
+console.log(myVoid);
